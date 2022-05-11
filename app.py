d33e95b80d7d0a616ca881adcc8b6882c09e844a
Oludolapo <Oketunjioludolapo1@gmail.com>
2022-05-11 22:42:59 +0100

    updated

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
-	application.run(debug=True) # running the app
+	application.run() # running the app
